4049c4b0808b33453ece51b3828717cccde5900c
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2025-09-14 13:17:42 +0800

    chore: make IndustryBaseApi.MODID deprecated

--- src/main/java/net/industrybase/api/IndustryBaseApi.java
+++ src/main/java/net/industrybase/api/IndustryBaseApi.java
@@ -3,5 +3,9 @@ package net.industrybase.api;
 import net.industrybase.futureenergy.FutureEnergy;
 
 public class IndustryBaseApi {
+	/**
+	 * @deprecated use {@link FutureEnergy#MODID}
+	 */
+	@Deprecated
 	public static final String MODID = FutureEnergy.MODID;
 }
