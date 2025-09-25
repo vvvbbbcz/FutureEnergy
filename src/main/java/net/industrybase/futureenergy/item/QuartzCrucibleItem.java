55423d258398fe01d64bf8280b225aade0aafcb0
vvvbbbcz <ms_vvvbbbcz@mail.scut.edu.cn>
2025-09-25 16:42:25 +0800

    feat: add quartz crucible

---
+++ src/main/java/net/industrybase/futureenergy/item/QuartzCrucibleItem.java
@@ -0,0 +1,9 @@
+package net.industrybase.futureenergy.item;
+
+import net.minecraft.world.item.Item;
+
+public class QuartzCrucibleItem extends Item implements ICrucible {
+	public QuartzCrucibleItem() {
+		super(new Properties());
+	}
+}
